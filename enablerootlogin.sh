8bb6d13ea02a822d6f9540920a110f3f2c4d066f
Ray <ray@truedays.org>
2013-11-07 05:08:14 -0600

    Update enablerootlogin.sh
    found newer version

--- enablerootlogin.sh
+++ enablerootlogin.sh
@@ -3,8 +3,7 @@
 # enablerootlogin.sh v.04  09292010
 # Ray@eboundhost.com
 #
-#  Temporarily enables root (password based) login for a period of 
-userset $sleeptime time.
+#  Temporarily enables root (password based) login for a period of userset $sleeptime time.
 #  remember to change the sshdconf file path in $sshdconf
 #
 # USAGE:  enablerootlogin.sh &
@@ -12,16 +11,13 @@ userset $sleeptime time.
 # Features:
 #   * Ignores comment lines in sshd_config
 #   * Reverts changes (re-secures) on user abort (Ctrl-C)
-#   * Continues even if user places in backgroud and logs out 
-(re-secures after $sleeptime)
+#   * Continues even if user places in backgroud and logs out (re-secures after $sleeptime)
 #
 # Bugs:
-#   * Will not run if sshd_config is not already set securely to 
-"without-password"
+#   * Will not run if sshd_config is not already set securely to "without-password"
 #
 # ToDo:
-#   * Test for cases of multiple lines beginning with PermitRoot...  
-Unknown results
+#   * Test for cases of multiple lines beginning with PermitRoot...  Unknown results
 #
 
 # Adjustable Variables:
@@ -31,15 +27,15 @@ sshdconf=/etc/ssh/sshd_config
 # Simple check to ensure sshd_config is how we expect
 if [ "`/bin/grep -e '^PermitRoot' $sshdconf`" != "PermitRootLogin 
 without-password" ] ; then
- echo -e "\nERROR--- Disable Root Login before using this script. 
+  echo -e "\nERROR--- Disable Root Login before using this script. 
 Expected: PermitRootLogin without-password"
-else
+ else
 
 # Trap the HUP signal to ignore it
-trap : HUP
+ trap : HUP
 
 # Trap Ctrl-C
-trap "echo -e '\n\nCANCELLED--please run this in the background if you 
+ trap "echo -e '\n\nCANCELLED--please run this in the background if you 
 with to continue working within this session with root login enabled.'" 
 SIGINT SIGTERM
 
